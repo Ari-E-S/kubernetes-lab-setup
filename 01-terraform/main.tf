dee2c53b4d20bf662fe7ccadce9becf7bc9980ff
Ariel Salvo <ariel.salvo@gmail.com>
2026-03-13 21:09:25 -0300

    Create a second controller
    Lower the RAM for all VMs

--- 01-terraform/main.tf
+++ 01-terraform/main.tf
@@ -9,12 +9,12 @@ module "k8s_controllers" {
   cloud_init     = templatefile("${path.module}/templates/user_data.yaml.tftpl", {
     public_key_ubuntu = tls_private_key.ed25519-key-ubuntu.public_key_openssh
   })
-  instance_count = 1
+  instance_count = 2
   image_name     = local.image_name
   name_prefix    = local.name_prefix
   name           = "controller"
   cpus           = 2
-  memory         = "4G"
+  memory         = "2G"
   disks          = "10G"
 }
 
@@ -29,6 +29,6 @@ module "k8s_workers" {
   name_prefix    = local.name_prefix
   name           = "worker"
   cpus           = 2
-  memory         = "4G"
+  memory         = "2G"
   disks          = "10G"
 }
