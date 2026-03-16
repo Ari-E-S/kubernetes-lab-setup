d38e304c506c35a288c96793e224b2e82b240468
Ariel Salvo <ariel.salvo@gmail.com>
2026-03-16 11:47:34 -0300

    Make platform drivers optional

--- 01-terraform/main.tf
+++ 01-terraform/main.tf
@@ -3,13 +3,18 @@ locals {
   image_name  = "noble"
 }
 
+locals {
+  cloud_init = templatefile("${path.module}/templates/user_data.yaml.tftpl", {
+    public_key_ubuntu = tls_private_key.ed25519-key-ubuntu.public_key_openssh,
+    platform           = "hyperv",
+  })
+}
+
 module "k8s_controllers" {
   source = "./modules/multipass"
 
-  cloud_init     = templatefile("${path.module}/templates/user_data.yaml.tftpl", {
-    public_key_ubuntu = tls_private_key.ed25519-key-ubuntu.public_key_openssh
-  })
-  instance_count = 2
+  cloud_init     = local.cloud_init
+  instance_count = 1
   image_name     = local.image_name
   name_prefix    = local.name_prefix
   name           = "controller"
@@ -21,10 +26,8 @@ module "k8s_controllers" {
 module "k8s_workers" {
   source = "./modules/multipass"
 
-  cloud_init     = templatefile("${path.module}/templates/user_data.yaml.tftpl", {
-    public_key_ubuntu = tls_private_key.ed25519-key-ubuntu.public_key_openssh
-  })
-  instance_count = 2
+  cloud_init     = local.cloud_init
+  instance_count = 1
   image_name     = local.image_name
   name_prefix    = local.name_prefix
   name           = "worker"
